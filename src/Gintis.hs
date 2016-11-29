8ac02b913f4a71d653e89c87344961151288db7c
alxemist <anthus.ab@gmail.com>
2016-11-29 15:51:00 +0300

    Pakeitimai

---
+++ src/Gintis.hs
@@ -0,0 +1,80 @@
+module Gintis
+where
+import Data.Char
+import Data.List
+import Network.HTTP.Client
+ 
+
+message :: String
+message = "d1:0d1:v1:x1:xi2e1:yi2ee1:1d1:v1:o1:xi0e1:yi1ee1:2d1:v1:x1:xi1e1:yi0eee"
+
+move :: String -> Maybe (Int, Int, Char)
+move str = calculteMove (pirmasZingsnis str)
+
+calculteMove :: [(Char,Char,Char)] -> Maybe(Int, Int, Char)
+calculteMove moves = findMove moves
+
+parseMove :: Maybe (Int, Int, Char) -> Maybe String
+parseMove Nothing = Nothing
+parseMove (Just (x,y,z)) = Just ("d1:v1:x1:xi" ++ [(intToDigit x)] ++  "e1:yi" ++ [(intToDigit y)] ++"eee")
+
+turn :: String -> Maybe String
+turn msg = parseMove $ move msg 
+
+findMove :: [(Char,Char,Char)] -> Maybe(Int, Int, Char)
+findMove moves  
+    | (filter(\(a,b,c)-> a=='1' && b=='1') moves)==[] = Just (digitToInt '1',digitToInt '1','x')
+    | (((length(moves))==2) && ((filter(\(a,b,c)-> a=='0' && b=='0') moves)==[])) = Just (digitToInt '0',digitToInt '0','x')
+    | (((length(moves))==2) && ((filter(\(a,b,c)-> a=='2' && b=='0') moves)==[])) = Just (digitToInt '2',digitToInt '0','x')
+    -- Kai x = 0
+    | (((length(filter(\(a,b,c)-> a=='0' && c=='o') moves))==2) && ((filter(\(a,b,c)-> a=='0' && c=='x') moves)==[]) && ((filter(\(a,b,c)-> a=='0' && b=='0') moves)==[])) = Just (digitToInt '0',digitToInt '0','x')
+    | (((length(filter(\(a,b,c)-> a=='0' && c=='o') moves))==2) && ((filter(\(a,b,c)-> a=='0' && c=='x') moves)==[]) && ((filter(\(a,b,c)-> a=='0' && b=='1') moves)==[])) = Just (digitToInt '0',digitToInt '1','x')
+    | (((length(filter(\(a,b,c)-> a=='0' && c=='o') moves))==2) && ((filter(\(a,b,c)-> a=='0' && c=='x') moves)==[]) && ((filter(\(a,b,c)-> a=='0' && b=='2') moves)==[])) = Just (digitToInt '0',digitToInt '2','x')
+    -- Kai x = 2
+    | (((length(filter(\(a,b,c)-> a=='2' && c=='o') moves))==2) && ((filter(\(a,b,c)-> a=='2' && c=='x') moves)==[]) && ((filter(\(a,b,c)-> a=='2' && b=='0') moves)==[])) = Just (digitToInt '2',digitToInt '0','x')
+    | (((length(filter(\(a,b,c)-> a=='2' && c=='o') moves))==2) && ((filter(\(a,b,c)-> a=='0' && c=='x') moves)==[]) && ((filter(\(a,b,c)-> a=='2' && b=='1') moves)==[])) = Just (digitToInt '2',digitToInt '1','x')
+    | (((length(filter(\(a,b,c)-> a=='2' && c=='o') moves))==2) && ((filter(\(a,b,c)-> a=='0' && c=='x') moves)==[]) && ((filter(\(a,b,c)-> a=='2' && b=='2') moves)==[])) = Just (digitToInt '2',digitToInt '2','x')
+    -- Kai y = 0
+    | (((length(filter(\(a,b,c)-> b=='0' && c=='o') moves))==2) && ((filter(\(a,b,c)-> b=='0' && c=='x') moves)==[]) && ((filter(\(a,b,c)-> a=='0' && b=='0') moves)==[])) = Just (digitToInt '0',digitToInt '0','x')
+    | (((length(filter(\(a,b,c)-> b=='0' && c=='o') moves))==2) && ((filter(\(a,b,c)-> b=='0' && c=='x') moves)==[]) && ((filter(\(a,b,c)-> a=='1' && b=='0') moves)==[])) = Just (digitToInt '1',digitToInt '0','x')
+    | (((length(filter(\(a,b,c)-> b=='0' && c=='o') moves))==2) && ((filter(\(a,b,c)-> b=='0' && c=='x') moves)==[]) && ((filter(\(a,b,c)-> a=='2' && b=='0') moves)==[])) = Just (digitToInt '2',digitToInt '0','x')
+    -- Kai y = 2
+    | (((length(filter(\(a,b,c)-> b=='2' && c=='o') moves))==2) && ((filter(\(a,b,c)-> b=='2' && c=='x') moves)==[]) && ((filter(\(a,b,c)-> a=='0' && b=='2') moves)==[])) = Just (digitToInt '0',digitToInt '2','x')
+    | (((length(filter(\(a,b,c)-> b=='2' && c=='o') moves))==2) && ((filter(\(a,b,c)-> b=='2' && c=='x') moves)==[]) && ((filter(\(a,b,c)-> a=='1' && b=='2') moves)==[])) = Just (digitToInt '1',digitToInt '2','x')
+    | (((length(filter(\(a,b,c)-> b=='2' && c=='o') moves))==2) && ((filter(\(a,b,c)-> b=='2' && c=='x') moves)==[]) && ((filter(\(a,b,c)-> a=='2' && b=='2') moves)==[])) = Just (digitToInt '2',digitToInt '2','x')
+    -- Tikrinami visi like atvejai
+    | (filter(\(a,b,c)-> a=='0' && b=='0') moves)==[] = Just (digitToInt '0',digitToInt '0','x')
+    | (filter(\(a,b,c)-> a=='2' && b=='0') moves)==[] = Just (digitToInt '2',digitToInt '0','x')
+    | (filter(\(a,b,c)-> a=='0' && b=='2') moves)==[] = Just (digitToInt '0',digitToInt '2','x')
+    | (filter(\(a,b,c)-> a=='2' && b=='2') moves)==[] = Just (digitToInt '2',digitToInt '2','x')
+    | (filter(\(a,b,c)-> a=='0' && b=='1') moves)==[] = Just (digitToInt '0',digitToInt '1','x')
+    | (filter(\(a,b,c)-> a=='1' && b=='0') moves)==[] = Just (digitToInt '1',digitToInt '0','x')
+    | (filter(\(a,b,c)-> a=='1' && b=='2') moves)==[] = Just (digitToInt '1',digitToInt '2','x')
+    | (filter(\(a,b,c)-> a=='2' && b=='1') moves)==[] = Just (digitToInt '2',digitToInt '1','x')
+    | otherwise = Nothing
+
+pirmasZingsnis :: String -> [(Char,Char,Char)]
+pirmasZingsnis "" = error "no list"
+pirmasZingsnis ('d' :rest) = antrasZingsnis (take ((length rest) -1) rest)
+pirmasZingsnis _ = error "netinkamas tipas"
+
+antrasZingsnis :: String -> [(Char,Char,Char)]
+antrasZingsnis "" = []
+antrasZingsnis a = treciasZingsnis a []
+
+treciasZingsnis :: String -> [(Char,Char,Char)] -> [(Char,Char,Char)]
+treciasZingsnis a [] = 
+    let
+        v = head (drop 9 a)
+        x = head (drop 14 a)
+        y = head (drop 20  a)
+        rest = drop 23 a
+    in treciasZingsnis rest ((x,y,v) : [])
+treciasZingsnis ('1' :a) acc =
+    let
+        v = head (drop 8 a)
+        x = head (drop 13 a)
+        y = head (drop 19  a)
+        rest = drop 22 a
+    in treciasZingsnis rest ((x,y,v):acc)
+treciasZingsnis "" acc = acc
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,58 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}
+module Main (main) where
 
-main :: IO ()
+import Gintis
+import Network.HTTP.Client
+import Network.HTTP.Types.Status (statusCode)
+import Network.HTTP.Types.Header
+import Data.Char
+import Data.Tuple.Select
+import qualified Data.ByteString.Char8 as B
+import Data.ByteString.Lazy.Char8 as C
+import Data.Maybe
+--import Data.ByteString.Lazy.pack as C
+urlSuper = "http://tictactoe.homedir.eu/game/antanas_test_13/player/1"
+
+post :: String -> IO()
+post ""= do
+  Prelude.putStrLn $ "Test: " ++ (("d1:0")++(fromJust (turn "d")))
+  manager <- newManager defaultManagerSettings
+  initialRequest <- parseUrlThrow urlSuper
+  let request = initialRequest { method = B.pack "POST",
+                                 requestHeaders = [(hContentType,B.pack "application/bencode+map"),(hAccept,B.pack "application/bencode+map")],
+                                 requestBody = RequestBodyLBS $ C.pack (("d1:0")++(fromJust (turn "d")))
+                               }
+  response <- httpLbs request manager
+  Prelude.putStrLn $ "The status code was: " ++ (show $ statusCode $ responseStatus response)
+  print $ responseBody response
+  get
+post msg= do
+  manager <- newManager defaultManagerSettings
+  initialRequest <- parseUrlThrow urlSuper
+  let request = initialRequest { method = B.pack "POST",
+                                 requestHeaders = [(hContentType,B.pack "application/bencode+map"),(hAccept,B.pack "application/bencode+map")],
+                                 requestBody = RequestBodyLBS $ C.pack ((Prelude.take ((Prelude.length msg) - 1) msg)++"1:"++[(intToDigit (div (Prelude.length(pirmasZingsnis msg)) 2))]++(fromJust (turn msg)))
+                               }
+  response <- httpLbs request manager
+  Prelude.putStrLn $ "The status code was atejo: " ++ (show $ statusCode $ responseStatus response)
+  Prelude.putStrLn $ msg
+  print $ responseBody response
+  get
+get :: IO()
+get = do
+  manager <- newManager defaultManagerSettings
+  initialRequest <- parseUrlThrow urlSuper
+  let request = initialRequest { method = "GET",
+                                 requestHeaders = [(hContentType, "application/bencode+map"),(hAccept, "application/bencode+map")]
+                               }
+  response <- httpLbs request manager
+  Prelude.putStrLn $ "The status code was1: " ++ (show $ statusCode $ responseStatus response)
+  post $ unpack $ responseBody response
+
+-- parseMove :: Maybe (Int, Int, Char) -> Maybe String
+-- parseMove Nothing = Nothing
+-- parseMove (Just (x,y,z)) = Just (" (m \"x\" " ++ [(intToDigit x)] ++ " \"y\" " ++ [(intToDigit y)] ++  "\"v\" \"" ++ [z] ++"\"))")
+
+main :: IO()
 main = do
-  putStrLn "hello world"
+    post ""
